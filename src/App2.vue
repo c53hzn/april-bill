8b0950fdcda19babfd62543a8d8b2e0b0568c965
Jenny <hzn2017@outlook.com>
2023-07-21 02:08:32 +0800

    Update App2.vue

--- src/App2.vue
+++ src/App2.vue
@@ -6,9 +6,6 @@
 
 <script>
 //firebase config start
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getFirestore,doc,getDocs,setDoc,deleteDoc,collection  } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 const firebaseConfig = {
   authDomain: process.env.VITE_authDomain,
   projectId: process.env.VITE_projectId,
